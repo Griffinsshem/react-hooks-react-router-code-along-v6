6b18bc192e674f1f00f3f20953e968779f59c887
Griffins Shem Ondeyo <griffinsshem254@gmail.com>
2025-07-16 22:47:35 +0300

    react hooks react router code along v6

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Home from "./pages/Home";
+// import to start implementing routes after installing react-router-dom(npm install react-router-dom@6 =>version 6)
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import routes from "./routes";
 
+// create a router object 
+const router = createBrowserRouter(routes);
+// render the RouterProvider which provides the router created by createBrowserRouter to our application so it can use React-Router's client side routing
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Home />);
+root.render(<RouterProvider router={router} />);
